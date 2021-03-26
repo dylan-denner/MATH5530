782ecd510afd78cb42cd986b5aded80cb832ea90
Dylan Denner <dd575213@ohio.edu>
2021-03-25 22:46:46 -0400

    correct data

--- SCRIPTS/AddingCountyRegion.R
+++ SCRIPTS/AddingCountyRegion.R
@@ -88,7 +88,64 @@ grouped_df <- joined_df %>%
             mean_in_school_suspensions = mean(`Students Disciplined - In-School Suspensions`, na.rm = TRUE),
             mean_total_students_discipline = mean(`total_students_discipline`, na.rm = TRUE))
 
+#####################################################################
 
+file_path <- "../DATA/mod_poverty_by_sex_edu_attainment.csv"
+ACS_data <- read.csv(file_path)
+
+file_path <- "../DATA/cleaning_districts_and_counties.csv"
+cleaned_names <- read.csv(file_path)
+
+cleaned_names <- cleaned_names[,-5:-12]
+cleaned_names <- cleaned_names[,-1:-2]
+
+names(cleaned_names)[1] <- "NAME"
+merged_data <- left_join(ACS_data, cleaned_names, by = "NAME")
+
+merged_data <- select(
+  merged_data,
+  NAME,
+  "District_Name.1",
+  "total_population",
+  "below_poverty_num",
+  "above_poverty_num",
+  "poverty_percentage",
+  "tot_less_than_HS",
+  "tot_HS",
+  "tot_some_college",
+  "tot_bachelor_plus",
+  "HS_PLUS_percentage"
+)
+
+
+merged_data[,2] <- str_sub(merged_data[,2], 1, nchar(merged_data[,2])-1)
+
+good_data <- subset(merged_data, grepl("(", merged_data$NAME, fixed=TRUE) == FALSE)
+bad_data <- subset(merged_data, grepl("(", merged_data$NAME, fixed=TRUE) == TRUE)
+
+names(grouped_df)[1] <- "District_Name"
+names(good_data)[2] <- "District_Name"
+
+df <- left_join(grouped_df, good_data, by = "District_Name")
+
+df <- df[,-11]
+
+#file_path_and_name = "../OUTPUT/doing_this_by_hand.csv"
+#write.csv(df, file_path_and_name)
+
+#file_path_and_name = "../OUTPUT/bad_data.csv"
+#write.csv(bad_data, file_path_and_name)
+#####################################################################
+# Reading in corrected data
+
+file_path <- "../DATA/doing_this_by_hand_complete.csv"
+pls_work <- read.csv(file_path)
+
+library(tidyr)
+testing <- pls_work %>% drop_na()
+
+file_path_and_name = "../OUTPUT/model_data_3_25.csv"
+write.csv(df, file_path_and_name)
 
 #####################################################################
 end.time <- Sys.time()
